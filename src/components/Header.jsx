ad29da065ecc0991cc02f726f0fadba0ed47333e
Artiom Bozieac <57958281+Yashmerino@users.noreply.github.com>
2022-11-30 15:13:39 +0200

    Add text in header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,22 @@
+import { Typography, Box } from "@mui/material";
 import React from "react";
 
 const Header = () => {
-  return <header></header>;
+  return (
+    <Box
+      display="flex"
+      justifyContent="center"
+      alignItems="center"
+      minHeight="25%"
+      mt="1%"
+      mr="2.5%"
+      ml="2.5%"
+      sx={{ flexDirection: 'column' }}
+    >
+      <Typography variant="h5" style={{fontWeight: 500, userSelect: "none", textAlign: "center" }}>Apo Alarm Clock</Typography>
+      <Typography variant="subtitle1" style={{lineHeight: 1, userSelect: "none", textAlign: "center"}}>Never be late again</Typography>
+    </Box>
+  );
 };
 
 export default Header;
