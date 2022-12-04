8d97d9a65e4c54ee56d77233b7baaf474f07b143
Artiom Bozieac <57958281+Yashmerino@users.noreply.github.com>
2022-12-04 20:04:07 +0200

    Change id

--- src/components/Clock/SleepButton.jsx
+++ src/components/Clock/SleepButton.jsx
@@ -7,7 +7,7 @@ const SleepButton = (props) => {
     e.preventDefault();
 
     let isValid = /^([0-9][0-9]|1[0-9]|2[0-3]):[0-5][0-9]$/.test(
-      document.getElementById(":r1:").value
+      document.getElementById(":r0:").value
     );
     if (!isValid) return;
 
