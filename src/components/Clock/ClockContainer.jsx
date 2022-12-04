3c2002b1081fba82efeb6aa4f64f72ce350d51f5
Artiom Bozieac <57958281+Yashmerino@users.noreply.github.com>
2022-12-04 02:16:04 +0200

    Add Console.log when alarm should go off

--- src/components/Clock/ClockContainer.jsx
+++ src/components/Clock/ClockContainer.jsx
@@ -1,9 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
 import { Box } from "@mui/material";
 import CustomTimePicker from "./CustomTimePicker";
 import SleepButton from "./SleepButton";
+import { returnCurrentTime } from "../../utils";
 
 const ClockContainer = (props) => {
+  const [value, setValue] = useState(null);
+  const [alarm, setAlarm] = useState(null);
+
+  const wakeUp = () => {
+    if (returnCurrentTime() === alarm) console.log("Wake up!");
+  };
+
+  setInterval(() => wakeUp(), 10000);
+
   return (
     <Box
       display="flex"
@@ -13,11 +23,16 @@ const ClockContainer = (props) => {
       mt="10%"
       sx={{ flexDirection: "column" }}
     >
-      <CustomTimePicker light={props.light} />
+      <CustomTimePicker
+        light={props.light}
+        value={value}
+        setValue={setValue}
+        setAlarm={setAlarm}
+      />
       <SleepButton
         setLight={props.setLight}
         light={props.light}
-        setAlarmTime={props.setAlarmTime}
+        alarm={alarm}
       />
     </Box>
   );
--- src/components/Clock/CustomTimePicker.jsx
+++ src/components/Clock/CustomTimePicker.jsx
@@ -1,35 +1,26 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import TextField from "@mui/material/TextField";
 import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
 import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
 import { TimePicker } from "@mui/x-date-pickers/TimePicker";
-import { returnCurrentTime } from "../../utils";
+import { returnCurrentTime, returnTime } from "../../utils";
 
 const CustomTimePicker = (props) => {
-  const [currentTime, setCurrentTime] = useState(returnCurrentTime());
-  const [value, setValue] = useState(null);
-
-  useEffect(() => {
-    const interval = setInterval(
-      () => setCurrentTime(returnCurrentTime()),
-      1000
-    );
-    return () => {
-      clearInterval(interval);
-    };
-  }, []);
-
   return (
     <LocalizationProvider dateAdapter={AdapterDayjs}>
       <TimePicker
-        label={"Current time: " + returnCurrentTime()}
-        value={value}
+        label={
+          props.light ? "Current time: " + returnCurrentTime() : "Sleep well :)"
+        }
+        value={props.value}
         ampm={false}
         onChange={(newValue) => {
-          setCurrentTime(newValue);
-          setValue(newValue);
+          props.setValue(newValue);
+          props.setAlarm(returnTime(newValue.$H, newValue.$m));
         }}
-        renderInput={(params) => <TextField {...params} sx={{ width: "25%" }}/>}
+        renderInput={(params) => (
+          <TextField {...params} sx={{ width: "25%" }} />
+        )}
         disabled={!props.light}
       />
     </LocalizationProvider>
